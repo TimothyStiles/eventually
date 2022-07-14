a1e96fa6160a4323a50da0b7b334c07d050a0a34
Timothy Stiles <tim@stiles.io>
2022-07-14 16:18:06 -0700

    main.go now sets payload url env variable.

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/the-gophers/go-action/pkg/parse"
+)
+
+func main() {
+	payloadURL, _ := parse.GetGithubActionPayloadURL()
+	os.Setenv("GITHUB_PAYLOAD_URL", payloadURL)
 }
